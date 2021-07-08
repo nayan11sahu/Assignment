edc6f9ada04e724b77d15917565cbbb12ffc463e
nayan11sahu <70404570+nayan11sahu@users.noreply.github.com>
2021-07-08 16:59:01 +0530

    Update Q7.js

--- Q7.js
+++ Q7.js
@@ -1,17 +1,17 @@
-const operatingSystem=[
-    {
-      Name:"Ubuntu" 
-    },
-     {
-      version:"18.4" 
-    },
-     {
+const operatingSystem=   {
+      Name:"Ubuntu" ,
+      version:"18.4" ,
       license:"open source" 
-    }
-  ]
-  console.log(operatingSystem);
+    };
+const arr=Object.entries(operatingSystem);
+console.log(arr);
+
 
 
 Output:
 
-[ { Name: 'Ubuntu' }, { version: '18.4' }, { license: 'open source' } ]
+[
+  [ 'Name', 'Ubuntu' ],
+  [ 'version', '18.4' ],
+  [ 'license', 'open source' ]
+]
